b26594480510c5a88b45c17005a1bcbe890537e8
andrewvh4 <31864022+andrewvh4@users.noreply.github.com>
2018-05-17 16:29:40 -0500

    Finished ToDos

--- ScienceBoardSoftware.ino/ScienceBoardSoftware/ScienceBoardSoftware.ino
+++ ScienceBoardSoftware.ino/ScienceBoardSoftware/ScienceBoardSoftware.ino
@@ -9,294 +9,281 @@
 
 
 
-#include "RoveComm.h"
-RoveCommEthernetUdp RoveComm;
+#include "RoveWare.h"
+#include "MPL3115A2.h"
 
 //Energia Libraries
 #include <Wire.h>
+#include <EthernetClient.h>
+#include <EthernetServer.h>
 
-#define USE_SERIAL 1 //Set to 1 to output sensor data to Serial
+#define USE_SERIAL_DEBUG 1 //Set to 1 to output sensor data to Serial
 
 ////////////////////
 //   Board Pins   //
 ////////////////////
+/////////////////////////////////////////////////
+const int SPECTROMETER_LASER_PIN          = PD_4;
+const int SPECTROMETER_MIRROR_ENCODER_PIN = PM_0; //pulseIn()
+const int SPECTROMETER_MIRROR_MOTOR_PIN_1 = PK_4;
+const int SPECTROMETER_MIRROR_MOTOR_PIN_2 = PK_5;
+const int SPECTROMETER_PHOTODIODE_PIN_1   = PD_0;
+const int SPECTROMETER_PHOTODIODE_PIN_2   = PD_1;
 
-const int LASER_PIN        = PD_4;
-const int ENCODER_PIN      = PM_0; //pulseIn()
-const int MIRROR_PIN_1     = PK_4;
-const int MIRROR_PIN_2     = PK_5;
-
-////////////////////
-//  Sensor Pins   //
-////////////////////
-const int PRESSURE_I2C     = 0;
-const int METHANE_PIN      = PE_0;
-const int AMMONIA_PIN      = PE_1;
-const int UV_PIN           = PE_2;
-const int HUMIDITY_PIN     = PE_3;
-const int TEMPERATURE_PIN  = PD_7;
-const int PHOTODIODE_PIN_1 = PD_0;
-const int PHOTODIODE_PIN_2 = PD_1;
-
-/////////////////////
-//Sensor Conversion//
-/////////////////////
-
-//Min/Max Value Input
-#define PRESSURE_MAX_ACTUAL      1
-#define PRESSURE_MAX_MEASURED    1
-#define PRESSURE_MIN_ACTUAL      1
-#define PRESSURE_MIN_MEASURED    1
-
-#define METHANE_MAX_ACTUAL       1
-#define METHANE_MAX_MEASURED     1
-#define METHANE_MIN_ACTUAL       1
-#define METHANE_MIN_MEASURED     1
-
-#define AMMONIA_MAX_ACTUAL       1
-#define AMMONIA_MAX_MEASURED     1
-#define AMMONIA_MIN_ACTUAL       1
-#define AMMONIA_MIN_MEASURED     1
-
-#define UV_MAX_ACTUAL            1
-#define UV_MAX_MEASURED          1
-#define UV_MIN_ACTUAL            1
-#define UV_MIN_MEASURED          1
-
-#define HUMIDITY_MAX_ACTUAL      1
-#define HUMIDITY_MAX_MEASURED    1
-#define HUMIDITY_MIN_ACTUAL      1
-#define HUMIDITY_MIN_MEASURED    1
-
-#define TEMPERATURE_MAX_ACTUAL   1
-#define TEMPERATURE_MAX_MEASURED 1
-#define TEMPERATURE_MIN_ACTUAL   1
-#define TEMPERATURE_MIN_MEASURED 1
+//Sensors/////////////////////////////////////////
+const int PRESSURE_I2C                    = 0;
+const int METHANE_PIN                     = PE_0;
+const int AMMONIA_PIN                     = PE_1;
+const int UV_PIN                          = PE_2;
+const int AIR_HUMIDITY_PIN                = PE_3;
+const int AIR_TEMPERATURE_PIN             = PD_7;
 
 ////////////////////
-//Conversion Math //
+// Sensor Config  //
 ////////////////////
+//Outputs///////////////////////////
+uint16_t air_pressure_pascals;
+uint16_t methane_ppm;
+uint16_t ammonia_ppm;
+uint16_t uv_intensity;
+uint16_t air_humidity_rh;
+uint16_t air_temperature_farenheit;
 
-//Scalar
-int pressure_Scalar    = (PRESSURE_MAX_ACTUAL    - PRESSURE_MIN_ACTUAL)   /(PRESSURE_MAX_MEASURED    - PRESSURE_MIN_MEASURED);              
-int methane_Scalar     = (METHANE_MAX_ACTUAL     - METHANE_MIN_ACTUAL)    /(METHANE_MAX_MEASURED     - METHANE_MIN_MEASURED);
-int ammonia_Scalar     = (AMMONIA_MAX_ACTUAL     - AMMONIA_MIN_ACTUAL)    /(AMMONIA_MAX_MEASURED     - AMMONIA_MIN_MEASURED);
-int uv_Scalar          = (UV_MAX_ACTUAL          - UV_MIN_ACTUAL)         /(UV_MAX_MEASURED          - UV_MIN_MEASURED);
-int humidity_Scalar    = (HUMIDITY_MAX_ACTUAL    - HUMIDITY_MIN_ACTUAL)   /(HUMIDITY_MAX_MEASURED    - HUMIDITY_MIN_MEASURED);
-int temperature_Scalar = (TEMPERATURE_MAX_ACTUAL - TEMPERATURE_MIN_ACTUAL)/(TEMPERATURE_MAX_MEASURED - TEMPERATURE_MIN_MEASURED);
-                        
-//Translational
-int pressure_Translational    = PRESSURE_MIN_ACTUAL    - (PRESSURE_MIN_MEASURED    * pressure_Scalar);
-int methane_Translational     = METHANE_MIN_ACTUAL     - (METHANE_MIN_MEASURED     * methane_Scalar);
-int ammonia_Translational     = AMMONIA_MIN_ACTUAL     - (AMMONIA_MIN_MEASURED     * ammonia_Scalar);
-int uv_Translational          = UV_MIN_ACTUAL          - (UV_MIN_MEASURED          * uv_Scalar);
-int humidity_Translational    = HUMIDITY_MIN_ACTUAL    - (HUMIDITY_MIN_MEASURED    * humidity_Scalar);
-int temperature_Translational = TEMPERATURE_MIN_ACTUAL - (TEMPERATURE_MIN_MEASURED * temperature_Scalar);
+//Map Consts/////////////////////////////////////////////////////////////////////////
+#define METHANE_MAX_ADC            1 //ADC
+#define METHANE_MAX_ACTUAL         1 //Real Sensor (Enter as Float, ie 1.234 => 1234)
+#define METHANE_MIN_ADC            1
+#define METHANE_MIN_ACTUAL         1
 
-////////////////////
-//Global Variables//
-////////////////////
+#define AMMONIA_MAX_ADC            1
+#define AMMONIA_MAX_ACTUAL         1
+#define AMMONIA_MIN_ADC            1
+#define AMMONIA_MIN_ACTUAL         1
 
-int pressure_Measurement;
-int methane_Measurement;
-int ammonia_Measurement;
-int uv_Measurement;
-int humidity_Measurement;
-int temperature_Measurement;
+#define UV_MAX_ADC                 1
+#define UV_MAX_ACTUAL              1
+#define UV_MIN_ADC                 1
+#define UV_MIN_ACTUAL              1
 
-uint16_t pressure_Output;
-uint16_t methane_Output;
-uint16_t ammonia_Output;
-uint16_t uv_Output;
-uint16_t humidity_Output;
-uint16_t temperature_Output;
+#define AIR_HUMIDITY_MAX_ADC       1
+#define AIR_HUMIDITY_MAX_ACTUAL    1
+#define AIR_HUMIDITY_MIN_ADC       1
+#define AIR_HUMIDITY_MIN_ACTUAL    1
 
+#define AIR_TEMPERATURE_MAX_ADC    1
+#define AIR_TEMPERATURE_MAX_ACTUAL 1
+#define AIR_TEMPERATURE_MIN_ADC    1
+#define AIR_TEMPERATURE_MIN_ACTUAL 1
 
 ////////////////////
 // RoveComm Setup //
 ////////////////////
+////////////////////////////////////////////////////////////////////
+RoveCommEthernetUdp RoveComm;
+EthernetServer      SpeectrometerTcpServer(ROVE_ETHERNET_TCP_PORT);
 
-//RoveComm Data_ID
-const uint16_t PRESSURE_ID         = 1824;
-const uint16_t METHANE_ID          = 1830;
-const uint16_t AMMONIA_ID          = 1842;
-const uint16_t UV_ID               = 1828;
-const uint16_t HUMIDITY_ID     = 1826;
-const uint16_t TEMPERATURE_ID  = 1824;
-const uint16_t COMMAND_ID          = 1808;
-
-//GetMessage Variables
+//Read Variables
 uint16_t data_id;
-uint16_t command_data_size;
-uint16_t command_data;
+size_t command_data_size;
+uint8_t data_value[2];
 
-const char PHOTODIODE_1_HEADER[] = "photodiode 1:";
-const char PHOTODIODE_2_HEADER[] = "photodiode 2:";
+MPL3115A2 PressureI2C;
 
-//Science Commands
-const uint16_t LASER_ON         = 2;
-const uint16_t LASER_OFF        = 3;
-const uint16_t SPECTROMETER_RUN = 6;
-
-//Ethernet Parameters
-const int SPECTROMETER_TCP_PORT = 11001;
-const int SCIENCE_BOARD_SUBNET  = 135;
-
-//Function Prottypes
+//Function Prototypes
 void spectrometerRun();
 
-void sendSensorValues();
-float getPressure();
-float getMethane();
-float getAmmonia();
-float getUVIntensity();
-float getHumidity();
-float getTemperature();
+void sensorSerialDebug();
+void spectrometerSerialDebug(uint16_t spectrometer_photodiode_1, uint16_t spectrometer_photodiode_2);
 
+
+//////////////////////////////////////////////////////////
 void setup() 
-{
-  //Initialize Pressure I2C 
-  Wire.setModule(PRESSURE_I2C);
+{  
   Wire.begin();
   delay(10);
-  
-  RoveComm.begin(192, 168, 1, SCIENCE_BOARD_SUBNET); 
+  PressureI2C.begin();
+  PressureI2C.setModeBarometer();
+  PressureI2C.setOversampleRate(7);
+  PressureI2C.enableEventFlags();
   delay(10);
-  if(USE_SERIAL)
+  RoveComm.begin(192, 168, 1, SCIENCEBOARD_FOURTH_OCTET); 
+  delay(10);
+  SpeectrometerTcpServer.begin();
+  delay(10);
+  
+  if(USE_SERIAL_DEBUG)
     Serial.begin(9600);
   delay(10);
 
-  pinMode(METHANE_PIN,      INPUT);
-  pinMode(AMMONIA_PIN,      INPUT);
-  pinMode(UV_PIN,           INPUT);
-  pinMode(HUMIDITY_PIN,     INPUT);
-  pinMode(TEMPERATURE_PIN,  INPUT);
-  pinMode(PHOTODIODE_PIN_1, INPUT);
-  pinMode(PHOTODIODE_PIN_2, INPUT);
-  pinMode(LASER_PIN,        INPUT);
-  pinMode(ENCODER_PIN,      INPUT);
+  pinMode(METHANE_PIN,                     INPUT);
+  pinMode(AMMONIA_PIN,                     INPUT);
+  pinMode(UV_PIN,                          INPUT);
+  pinMode(AIR_HUMIDITY_PIN,                INPUT);
+  pinMode(AIR_TEMPERATURE_PIN,             INPUT);
   
-  pinMode(MIRROR_PIN_1,    OUTPUT);
-  pinMode(MIRROR_PIN_2,    OUTPUT);
-
- EthernetServer SpectrometerTCP(SPECTROMETER_TCP_PORT);
+  pinMode(SPECTROMETER_PHOTODIODE_PIN_1,   INPUT);
+  pinMode(SPECTROMETER_PHOTODIODE_PIN_2,   INPUT);
+  pinMode(SPECTROMETER_LASER_PIN,          OUTPUT);
+  pinMode(SPECTROMETER_MIRROR_ENCODER_PIN, OUTPUT);
+  pinMode(SPECTROMETER_MIRROR_MOTOR_PIN_1, OUTPUT);
+  pinMode(SPECTROMETER_MIRROR_MOTOR_PIN_2, OUTPUT);
 
  //ToDo:Watchdog
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop() 
 {
-  roveComm.getMessage(data_id, command_data_size, command_data);
+  RoveComm.read(&data_id, &command_data_size, data_value);
 
-  switch case(data_id)
+  switch (data_id)
   {
     case(LASER_ON):
-      break;/////////////////ToDo
+      digitalWrite(SPECTROMETER_LASER_PIN, HIGH);
+      break;
     case(LASER_OFF):
-      break;/////////////////ToDo
-    case(SPECTROMETER_RUN)
+      digitalWrite(SPECTROMETER_LASER_PIN, LOW);
+      break;
+    case(SPECTROMETER_RUN):
       spectrometerRun();
       break;
     default:
+      break;
   }
 
-  //Read and send sensor values
-  sendSensorValues();
+  //Read and Map sensor Vales
+  air_pressure_pascals       = PressureI2C.readPressure();
+  methane_ppm                = 0.001*map(analogRead(METHANE_PIN),         METHANE_MIN_ADC,         METHANE_MAX_ADC,         METHANE_MIN_ACTUAL,         METHANE_MAX_ACTUAL);
+  ammonia_ppm                = 0.001*map(analogRead(AMMONIA_PIN),         AMMONIA_MIN_ADC,         AMMONIA_MAX_ADC,         AMMONIA_MIN_ACTUAL,         AMMONIA_MAX_ACTUAL);
+  uv_intensity               = 0.001*map(analogRead(UV_PIN),              UV_MIN_ADC,              UV_MAX_ADC,              UV_MIN_ACTUAL,              UV_MAX_ACTUAL);
+  air_humidity_rh            = 0.001*map(analogRead(AIR_HUMIDITY_PIN),    AIR_HUMIDITY_MIN_ADC,    AIR_HUMIDITY_MAX_ADC,    AIR_HUMIDITY_MIN_ACTUAL,    AIR_HUMIDITY_MAX_ACTUAL);
+  air_temperature_farenheit  = 0.001*map(analogRead(AIR_TEMPERATURE_PIN), AIR_TEMPERATURE_MIN_ADC, AIR_TEMPERATURE_MAX_ADC, AIR_TEMPERATURE_MIN_ACTUAL, AIR_TEMPERATURE_MAX_ACTUAL);
+
+  //Send Senor Values
+  //RoveComm.write(AIR_PRESSURE_SCI_SENSOR_0,    sizeof(float), PressureI2C To Do      );
+
+  RoveComm.write(AIR_PRESSURE_SCI_SENSOR_0,    sizeof(float), &air_pressure_pascals       );
+  RoveComm.write(METHANE_SCI_SENSOR_1,         sizeof(float), &methane_ppm                );
+  RoveComm.write(AMMONIA_SCI_SENSOR_2,         sizeof(float), &ammonia_ppm                );
+  RoveComm.write(UV_SCI_SENSOR_3,              sizeof(float), &uv_intensity               );
+  RoveComm.write(AIR_HUMIDITY_SCI_SENSOR_4,    sizeof(float), &air_humidity_rh            );
+  RoveComm.write(AIR_TEMPERATURE_SCI_SENSOR_5, sizeof(float), &air_temperature_farenheit  );
+
+  if(USE_SERIAL_DEBUG) sensorSerialDebug();
 }
 
-////////////////////////////ToDo
+/////////////////////////////////////////////////////
 void spectrometerRun()
-{}
-
-sendSensorValues()
 {
-  roveComm.write(PRESSURE_ID,    sizeof(float), getPressure()   );
-  roveComm.write(METHANE_ID,     sizeof(float), getMethane()    );
-  roveComm.write(AMMONIA_ID,     sizeof(float), getAmmonia()    );
-  roveComm.write(UV_ID,          sizeof(float), getUV()         );
-  roveComm.write(HUMIDITY_ID,    sizeof(float), getHumidity()   );
-  roveComm.write(TEMPERATURE_ID, sizeof(float), getTemperature());
-  
-  return;
-}
+  EthernetClient SpectrometerTcpClient = SpeectrometerTcpServer.available();
 
+  if(SpectrometerTcpClient)
+  {
+     uint16_t spectrometer_photodiode_1;
+     uint16_t spectrometer_photodiode_2;
+     String tcp_buffer;
+
+     //Move Spectrometer Motor Forward
+     digitalWrite(SPECTROMETER_MIRROR_MOTOR_PIN_1, HIGH);
+     digitalWrite(SPECTROMETER_MIRROR_MOTOR_PIN_1, LOW );
+
+     delay(5000);
+
+     //Turn on Laser
+     digitalWrite(SPECTROMETER_LASER_PIN, HIGH);
+
+     //Loop takes analog reading, delays 1/8 second, runs for roughly 30 seconds
+     for(int i = 0; i<240; i++)
+     {
+      //Read Photodiodes
+      spectrometer_photodiode_1 = analogRead(SPECTROMETER_PHOTODIODE_PIN_1);
+      spectrometer_photodiode_2 = analogRead(SPECTROMETER_PHOTODIODE_PIN_2);
+
+      if(USE_SERIAL_DEBUG) spectrometerSerialDebug(spectrometer_photodiode_1, spectrometer_photodiode_2);
+
+      //Print Data
+      SpeectrometerTcpServer.println("photodiode 1:");
+      SpeectrometerTcpServer.println(spectrometer_photodiode_1);
+      SpeectrometerTcpServer.println("photodiode 2:");
+      SpeectrometerTcpServer.println(spectrometer_photodiode_2);
+      
+      delay(125);
+     }
+
+     //Turn off Laser
+     digitalWrite(SPECTROMETER_LASER_PIN, LOW);
+
+     //Stop Motor
+     digitalWrite(SPECTROMETER_MIRROR_MOTOR_PIN_1, LOW);
+     digitalWrite(SPECTROMETER_MIRROR_MOTOR_PIN_1, LOW);
+
+     //End TCP Client
+     SpectrometerTcpClient.stop();
+
+     delay(2000);
+
+     //Return Motor to Start
+     digitalWrite(SPECTROMETER_MIRROR_MOTOR_PIN_1, LOW );
+     digitalWrite(SPECTROMETER_MIRROR_MOTOR_PIN_1, HIGH);
+     delay(40000);
+
+     //Stop Motor
+     digitalWrite(SPECTROMETER_MIRROR_MOTOR_PIN_1, LOW);
+     digitalWrite(SPECTROMETER_MIRROR_MOTOR_PIN_1, LOW);
+
+     delay(10000);
+  }
+}
 
 ////ToDo
 float getPressure()
 {}
 
-float getMethane()
+//////////////////////////////////
+void sensorSerialDebug()
 {
-  methane_Measurement = analogRead(METHANE_PIN);
-  methane_Output = methane_Measurement * methane_Scalar = methane_Translational;
-  if(USE_SERIAL)
-  {
+    Serial.println("");
+    Serial.print("Pressure Out");
+    Serial.println(air_pressure_pascals);
+    
     Serial.println("");
     Serial.print("Methane Ain:");
-    Serial.println(methane_Measurement);
-    Serial.print("Methane Out")l
-    Serial.println(methane_Output
-  }
-  return(methane_Output);
-}
+    Serial.println(analogRead(METHANE_PIN));
+    Serial.print("Methane Out");
+    Serial.println(methane_ppm);
 
-float getAmmonia()
-{
-  ammonia_Measurement = analogRead(AMMONIA_PIN);
-  ammonia_Output = ammonia_Measurement * ammonia_Scalar = ammonia_Translational;
-  if(USE_SERIAL)
-  {
     Serial.println("");
     Serial.print("Ammonia Ain:");
-    Serial.println(ammonia_Measurement);
-    Serial.print("Ammonia Out")l
-    Serial.println(ammonia_Output
-  }
-  return(ammonia_Output);
-}
+    Serial.println(analogRead(AMMONIA_PIN));
+    Serial.print("Ammonia Out");
+    Serial.println(ammonia_ppm);
 
-float getUVIntensity()
-{
-  uv_Measurement = analogRead(UV_PIN);
-  uv_Output = uv_Measurement * uv_Scalar = uv_Translational;
-  if(USE_SERIAL)
-  {
     Serial.println("");
     Serial.print("UV Ain:");
-    Serial.println(uv_Measurement);
-    Serial.print("UV Out")l
-    Serial.println(uv_Output
-  }
-  return(uv_Output);
+    Serial.println(analogRead(UV_PIN));
+    Serial.print("UV Out");
+    Serial.println(uv_intensity);
+
+    Serial.println("");
+    Serial.print("Air Humidity Ain:");
+    Serial.println(analogRead(AIR_HUMIDITY_PIN));
+    Serial.print("Air Humidity Out");
+    Serial.println(air_humidity_rh);
+
+    Serial.println("");
+    Serial.print("Air Temperature Ain:");
+    Serial.println(analogRead(AIR_TEMPERATURE_PIN));
+    Serial.print("Air Temperature Out");
+    Serial.println(air_temperature_farenheit);
+
+    return;
 }
 
-float getHumidity()
+void spectrometerSerialDebug(uint16_t spectrometer_photodiode_1, uint16_t spectrometer_photodiode_2)
 {
-  humidity_Measurement = analogRead(HUMIDITY_PIN);
-  humidity_Output = humidity_Measurement * humidity_Scalar = humidity_Translational;
-  if(USE_SERIAL)
-  {
-    Serial.println("");
-    Serial.print("Humidity Ain:");
-    Serial.println(humidity_Measurement);
-    Serial.print("Humidity Out")l
-    Serial.println(humidity_Output
-  }
-  return(humidity_Output);
-}
-
-float getTemperature()
-{
-  temperature_Measurement = analogRead(TEMPERATURE_PIN);
-  temperature_Output = temperature_Measurement * temperature_Scalar = temperature_Translational;
-  if(USE_SERIAL)
-  {
-    Serial.println("");
-    Serial.print("Temperature Ain:");
-    Serial.println(temperature_Measurement);
-    Serial.print("Temperature Out")l
-    Serial.println(temperature_Output
-  }
-  return(temperature_Output);
+  Serial.print("Photodiode 1: ");
+  Serial.println(spectrometer_photodiode_1);
+  Serial.print("Photodiode 2: ");
+  Serial.println(spectrometer_photodiode_2);
 }
 
